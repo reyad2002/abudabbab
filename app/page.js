827e4b8238fad5327d4aa4479614c6038a95dcf4
Reyad Mohamed Fathy <reyadmohamed631@gmail.com>
2025-09-21 04:06:52 +0300

    Refactor checkout section to streamline booking logic and improve component structure; remove unused imports and enhance rendering conditions for better performance.

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
 import TripsSection from "./trips/page";
-// import TripInfo from "../_components/pages/tripInfo.jsx";
-// import TripMoreInfo from "../_components/pages/tripMoreInfo.jsx";
+
 export default function Home() {
-  return <div className="">
-    <TripsSection/>
-    {/* <TripInfo/> */}
-    {/* <TripMoreInfo/> */}
-  </div>;
+  return (
+    <div className="">
+      <TripsSection />
+    </div>
+  );
 }
